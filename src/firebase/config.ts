9d0fba7e00e3ae8b62322a194ad635e05b922064
뭉치타정 <jeongmungchita@gmail.com>
2025-10-10 18:43:01 +0000

    'studio-2481293716-bdd83.appspot.com'<-------이거 안쓰고!!!!!!!!!! studio-248

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,7 +4,7 @@ export const firebaseConfig = {
   "appId": "1:279447898825:web:d31e0ec9bc493c95031b18",
   "apiKey": "AIzaSyAddKQplS9B7OG0o-WPcjpYRKwFh6dOkDs",
   "authDomain": "studio-2481293716-bdd83.firebaseapp.com",
-  "storageBucket": "studio-2481293716-bdd83.appspot.com",
+  "storageBucket": "studio-2481293716-bdd83.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "279447898825"
 };
