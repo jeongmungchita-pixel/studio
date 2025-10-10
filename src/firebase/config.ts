77605fc3b1571541fb84ac4599d5a40042e9edb5
뭉치타정 <jeongmungchita@gmail.com>
2025-10-10 15:32:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2481293716-bdd83",
+  "appId": "1:279447898825:web:d31e0ec9bc493c95031b18",
+  "apiKey": "AIzaSyAddKQplS9B7OG0o-WPcjpYRKwFh6dOkDs",
+  "authDomain": "studio-2481293716-bdd83.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "279447898825"
+};
